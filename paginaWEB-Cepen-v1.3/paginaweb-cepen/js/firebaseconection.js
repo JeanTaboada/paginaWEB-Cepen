794d581c3debb1bbbf11026be74ea665eff250a1
Jean Taboada Peralta <119762753+JeanTaboada@users.noreply.github.com>
2024-08-01 17:03:54 -0500

    Versión 1.3 se agrego Login + css
    se agrego correctamente, manda error pero no reconoce el usuario en Firebase

---
+++ paginaWEB-Cepen-v1.3/paginaweb-cepen/js/firebaseconection.js
@@ -0,0 +1,43 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-analytics.js";
+import { getAuth, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyD8-LBXVhB1w8ok_bP3L2Q3kchf2DCU8',
+    authDomain: 'crud-cepen-web.firebaseapp.com',
+    projectId: 'crud-cepen-web',
+    storageBucket: 'crud-cepen-web.appspot.com',
+    messagingSenderId: '625555156658',
+    appId: '1:625555156658:web:5fc29488466a92d76063ab',
+}
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth();
+
+export class ManageAccount {
+
+    authenticate(email, password) {
+        signInWithEmailAndPassword(auth, email, password)
+            .then((_) => {
+                window.location.href = "index.html";
+                // Mostrar alerta de inicio de sesión exitoso
+                alert("Has iniciado sesión correctamente. Serás redirigido a la página principal.");
+            })
+            .catch((error) => {
+                console.error(error.message);
+                // Mostrar alerta de error de inicio de sesión
+                alert("Error al iniciar sesión: " + error.message);
+            });
+    }
+
+    signOut() {
+        signOut(auth)
+            .then((_) => {
+                window.location.href = "index.html";
+            })
+            .catch((error) => {
+                console.error(error.message);
+            });
+    }
+}
